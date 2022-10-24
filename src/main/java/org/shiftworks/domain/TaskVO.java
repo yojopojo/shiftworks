f200be1216ea378cf92c63c0860a88fd49385110
cocobini <chaebin421@gmail.com>
2022-10-24 15:36:06 +0900

    feat: Add VO
    Add TaskVO, TaskFileVO, TaskChargeVO

--- src/main/java/org/shiftworks/domain/TaskVO.java
+++ src/main/java/org/shiftworks/domain/TaskVO.java
@@ -1,5 +1,26 @@
 package org.shiftworks.domain;
 
-public class TaskVO {
+import java.util.Date;
+import java.util.List;
 
+import lombok.Data;
+
+@Data
+public class TaskVO {
+	
+	private Integer task_id;
+	private String dept_id;
+	private String task_title;
+	private String task_content;
+	private String task_writer;
+	private Date start_date;
+	private Date end_date;
+	private String category;
+	private Character t_priavte;
+	private Character notification;
+	private Date date_created;
+	
+	private List<TaskFileVO> fileList;
+	private List<TaskChargeVO> chargeList;
+	
 }
