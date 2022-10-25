c60b4760651cb5ed276ddd1633b8bf172bc1c0e1
jh1359753 <jh1359753@gmail.com>
2022-10-25 16:29:56 +0900

    push test : BookingVO 복구

--- src/main/java/org/shiftworks/domain/BookingVO.java
+++ src/main/java/org/shiftworks/domain/BookingVO.java
@@ -1,6 +1,14 @@
 package org.shiftworks.domain;
 
 public class BookingVO {
-	//test local to remote
+	private int book_id;
+	private String rsc_id;
+	private String emp_id;
+	private String dept_id;
+	private String book_date;
+	private int book_begin;
+	private int book_end;
+	private String book_title;
+	private String book_content;
 	
 }
