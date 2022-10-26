68fa601ad1c9fdd9b5799e80c0851f4211d66a4f
cocobini <chaebin421@gmail.com>
2022-10-26 10:20:11 +0900

    feat: Add mapper
    add schedule mapper

--- src/main/java/org/shiftworks/domain/ScheduleCriteria.java
+++ src/main/java/org/shiftworks/domain/ScheduleCriteria.java
@@ -2,4 +2,20 @@ package org.shiftworks.domain;
 
 public class ScheduleCriteria {
 	
+	private String selectedDate;
+	private String group;
+	
+	public ScheduleCriteria() {
+		
+	}
+
+	public ScheduleCriteria(String selectedDate, String group) {
+		super();
+		this.selectedDate = selectedDate;
+		this.group = group;
+	}
+	
+	public String[] getTime(String selectedDate) {
+		return selectedDate == null ? new String[] {}: selectedDate.split("-");
+	}
 }
---
+++ src/test/java/org/shiftworks/mapper/ScheduleMapperTests.java
@@ -0,0 +1,14 @@
+package org.shiftworks.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScheduleMapperTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
