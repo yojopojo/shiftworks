6aaf0884834bd9204af6bbcd1f462dd8cbdade99
jiyoung yu <jyjyu1204@gmail.com>
2022-10-28 16:44:34 +0900

    feat LOG...
    add CustomDetailsService , CustomUser.java

--- src/main/java/org/shiftworks/security/CustomUserDetailsService.java
+++ src/main/java/org/shiftworks/security/CustomUserDetailsService.java
@@ -1,5 +1,7 @@
 package org.shiftworks.security;
 
+import org.shiftworks.mapper.EmployeeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,8 +11,13 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class CustomUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private EmployeeMapper empMapper;
+	
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		
+		log.warn("Load User By UserName: " + userName);
 		
 		return null;
 	}
--- src/test/java/org/shiftworks/security/UserTests.java
+++ src/test/java/org/shiftworks/security/UserTests.java
@@ -18,7 +18,10 @@ import lombok.extern.log4j.Log4j;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration({
+	  "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	  "file:src/main/webapp/WEB-INF/spring/security-context.xml"
+	  })
 @Log4j
 public class UserTests {
 	
@@ -31,9 +34,9 @@ public class UserTests {
 	@Test
 	public void testInsertUser() {
 		
-		String sql = "insert into employee(emp_id, password, name) values (?, ?, ?)";
+		String sql = "insert into employee(emp_id, password, name, dept_id) values (?, ?, ?, ?)";
 		
-		for(int i=7;i<100;i++) {
+		for(int i=1;i<=10;i++) {
 			
 			Connection con = null;
 			PreparedStatement pstmt = null;
@@ -43,22 +46,25 @@ public class UserTests {
 				con = ds.getConnection();
 				pstmt = con.prepareStatement(sql);
 				
-				pstmt.setString(2, pwencoder.encode("pw" + i));
+				pstmt.setString(2, pwencoder.encode("shiftworks113"));
 				
-				if(i<80) {
+				if(i<7) {
 					
 					pstmt.setString(1, "user"+i);
 					pstmt.setString(3, "name"+i);
+					pstmt.setString(4, "dept1");
 					
-				}else if(i<90){
+				}else if(i<8){
 					
 					pstmt.setString(1, "manager"+i);
 					pstmt.setString(3, "mngname"+i);
+					pstmt.setString(4, "dept2");
 					
 				}else {
 					
 					pstmt.setString(1, "admin"+i);
 					pstmt.setString(3, "admname"+i);
+					pstmt.setString(4, "infosecu1");
 				}
 				
 				pstmt.executeUpdate();
@@ -71,6 +77,49 @@ public class UserTests {
 	        
 			}
 		}//end for
-	}
+	}//인증, 권한 테스트를 위한 employee 테이블에 dummy data insert
 
+//	@Test
+//	  public void testInsertAuth() {
+//	    
+//	    
+//	    String sql = "insert into department (dept_id, dept_name, authority) values (?,?,?)";
+//	    
+//	    for(int i = 2; i < 6; i++) {
+//	      
+//	      Connection con = null;
+//	      PreparedStatement pstmt = null;
+//	      
+//	      try {
+//	        con = ds.getConnection();
+//	        pstmt = con.prepareStatement(sql);
+//	      
+//	        
+//	        if(i <5) {
+//	          
+//	          pstmt.setString(1, "dept"+i);
+//	          pstmt.setString(2, "normal");
+//	          pstmt.setString(3,"user");
+//	          
+//	        }else {
+//	          
+//	        	   pstmt.setString(1, "infosecu2");
+//	        	   pstmt.setString(2, "secu111");
+//	 	          pstmt.setString(3,"admin");
+//	          
+//	        }
+//	        
+//	        pstmt.executeUpdate();
+//	        
+//	      }catch(Exception e) {
+//	        e.printStackTrace();
+//	      }finally {
+//	        if(pstmt != null) { try { pstmt.close();  } catch(Exception e) {} }
+//	        if(con != null) { try { con.close();  } catch(Exception e) {} }
+//	        
+//	      }
+//	    }//end for
+//	  }//인증, 권한 테스트를 위한 departmet 테이블에 dummy data insert
+
+	
 }
