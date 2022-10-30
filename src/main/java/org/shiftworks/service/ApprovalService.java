674d7470083c8491cc4e1ce304c400988fc4ffc5
sujin kim <sjvenus94@gmail.com>
2022-10-31 01:53:20 +0900

    ADD ApprovalDTO
    FIX apr_status/af_id String으로 변환

--- src/main/java/org/shiftworks/service/ApprovalService.java
+++ src/main/java/org/shiftworks/service/ApprovalService.java
@@ -2,6 +2,7 @@ package org.shiftworks.service;
 
 import java.util.List;
 
+import org.shiftworks.domain.ApprovalDTO;
 import org.shiftworks.domain.ApprovalVO;
 import org.shiftworks.domain.TempApprovalVO;
 
@@ -10,8 +11,9 @@ public interface ApprovalService {
 	// 결재 
 	public int insertForm(ApprovalVO approval);
 	public ApprovalVO get(int apr_id);
-	public List<ApprovalVO> getList();
+	public List<ApprovalDTO> getList();
 	public boolean update(ApprovalVO approval);
 	public void updateStatus(int apr_id, String status);
-
+	
+	
 }
--- src/main/java/org/shiftworks/service/ApprovalServiceImpl.java
+++ src/main/java/org/shiftworks/service/ApprovalServiceImpl.java
@@ -1,7 +1,10 @@
 package org.shiftworks.service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.shiftworks.domain.ApprovalDTO;
 import org.shiftworks.domain.ApprovalVO;
 import org.shiftworks.mapper.ApprovalMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,8 +32,49 @@ public class ApprovalServiceImpl implements ApprovalService {
 	}
 
 	@Override
-	public List<ApprovalVO> getList() {
-		return mapper.getList();
+	public List<ApprovalDTO> getList() {
+		List<ApprovalVO> list = mapper.getList();
+		List<ApprovalDTO> dtos = new ArrayList<ApprovalDTO>();
+		
+		// 결재 상태, 결재 양식 String으로 변환
+		for (ApprovalVO approval : list) {
+			
+			String status = "";			
+			if (approval.getApr_status() == '0') {
+				status = "대기중";
+			} else if (approval.getApr_status()=='1') {
+				status = "승인";
+			} else if (approval.getApr_status()=='2'){
+				status = "반려";
+			} else {
+				status = "완료";
+			}
+			
+			String af_id = "";
+			if (approval.getAf_id()== 1) {
+				af_id = "기안서";
+			} else if (approval.getAf_id()== 2) {
+				af_id = "품의서";
+			} else if (approval.getAf_id()== 3) {
+				af_id = "휴가신청서";
+		}
+			
+			// builder 패턴 이용 
+			ApprovalDTO dto = ApprovalDTO.builder()
+			.apr_id(approval.getApr_id())
+			.af_id(af_id) // 위에서 변환했기 때문에 approval에서 가져올 필요 없음
+			.emp_id(approval.getEmp_id())
+			.apr_receivedate(approval.getApr_receivedate())
+			.apr_title(approval.getApr_title())
+			.apr_content(approval.getApr_content())
+			.apr_status(status) // 위에서 변환했기 때문에 approval에서 가져올 필요 없음
+			.apr_comment(approval.getApr_comment())
+			.apr_signdate(approval.getApr_signdate())
+			
+			.build();	
+			dtos.add(dto);
+		}				
+		return dtos;
 	}
 
 	@Override
@@ -44,6 +88,9 @@ public class ApprovalServiceImpl implements ApprovalService {
 		mapper.updateStatus(apr_id, status);
 	}
 
+	
+
+	
 	
 
 }
