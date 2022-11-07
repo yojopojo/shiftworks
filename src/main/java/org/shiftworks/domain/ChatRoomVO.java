65bb42b4cf6b8ceb53faa36cce44ac6507bd1345
haerim kwon <podo9264@gmail.com>
2022-11-07 12:48:54 +0900

    feat : build stomp setting.
    - ALTER TABLE chatroom Modify room_id VARCHAR2(100);
    - int room_id -> String room_id (uuid)

--- src/main/java/org/shiftworks/domain/ChatRoomVO.java
+++ src/main/java/org/shiftworks/domain/ChatRoomVO.java
@@ -1,13 +1,23 @@
 package org.shiftworks.domain;
 
+import java.util.UUID;
+
 import lombok.Data;
 
 @Data
 public class ChatRoomVO {
-	private int room_id;
+	private String room_id;
 	private String room_name;
 	private String lastchat;
 	private String lastchat_time;
 	private String emp_id;
 	private String dept_id;
+	
+	public static ChatRoomVO create(String name) {
+		ChatRoomVO chatRoom = new ChatRoomVO();
+		chatRoom.room_id = UUID.randomUUID().toString();
+		chatRoom.room_name = name;
+		return chatRoom;
+	}
+	
 }
---
+++ src/main/java/org/shiftworks/handler/WebSocketController.java
@@ -0,0 +1,16 @@
+package org.shiftworks.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+	@MessageMapping("/stomp")
+	@SendTo("/topic/stomp")
+	public ResponseEntity<String> stomp(String request) {
+		return new ResponseEntity<String>(request, HttpStatus.OK);
+	}
+}
